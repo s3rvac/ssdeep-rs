726e77cf8f2dd741721110a773ad8867ca35c45a
Petr Zemek <s3rvac@gmail.com>
2017-05-23 20:02:21 +0200

    Add some basic documentation for the underlying libfuzzy-sys crate.
    Just in case someone stumbles upon it.

--- libfuzzy-sys/lib.rs
+++ libfuzzy-sys/lib.rs
@@ -15,6 +15,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+//! Native bindings to the `libfuzzy` library, which is internally used by the
+//! [ssdeep-rs](https://github.com/s3rvac/ssdeep-rs) crate. See its description
+//! for more information.
+
 extern crate libc;
 
 use libc::c_char;
@@ -24,19 +28,22 @@ use libc::uint32_t;
 
 // From fuzzy.h:
 
-// Length of an individual fuzzy hash signature component.
+/// Length of an individual fuzzy hash signature component.
 const SPAMSUM_LENGTH: usize = 64;
 
-// The longest possible length for a fuzzy hash signature.
+/// The longest possible length for a fuzzy hash signature.
 pub const FUZZY_MAX_RESULT: usize = 2 * SPAMSUM_LENGTH + 20;
 
 extern "C" {
+    /// Computes the match score between two fuzzy hashes.
     // int fuzzy_compare(const char *sig1, const char *sig2);
     pub fn fuzzy_compare(sig1: *const c_char, sig2: *const c_char) -> c_int;
 
+    /// Computes the fuzzy hash of a buffer.
     // int fuzzy_hash_buf(const unsigned char *buf, uint32_t buf_len, char *result);
     pub fn fuzzy_hash_buf(buf: *const c_uchar, buf_len: uint32_t, result: *mut c_char) -> c_int;
 
+    /// Computes the fuzzy hash of a file.
     // int fuzzy_hash_filename(const char *filename, char *result);
     pub fn fuzzy_hash_filename(filename: *const c_char, result: *mut c_char) -> c_int;
 }
